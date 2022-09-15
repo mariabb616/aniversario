711e42b6071bd59f79eb04bbcce3f3cb0898a91c
Jolie <56496340+mariabb616@users.noreply.github.com>
2022-09-14 21:45:07 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1663202596,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"botaosim-sheet0.png",
+		"sprite-sheet0.png",
+		"reiniciar2-sheet0.png",
+		"limite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"video.mp4",
+		"video2.mp4"
+	]
+}
